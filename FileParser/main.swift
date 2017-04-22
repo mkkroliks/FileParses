a8e3a0cff37b4790d6bcf048bcbe607a4218dfa8
Maciej Krolikowski <mkkrolik@gmail.com>
2017-04-22 17:54:50 +0200

    gathering all needed value in arrays

--- FileParser/main.swift
+++ FileParser/main.swift
@@ -10,3 +10,44 @@ import Foundation
 
 print("Hello, World!")
 
+
+    do {
+        let data = try String(contentsOfFile: "/Users/mk/FileParser/j10/j102_2.mm", encoding: .utf8)
+        let lines = data.components(separatedBy: .newlines)
+        
+        var readyToRead = false
+        var endOfData = false
+        
+        var dataLines = lines.filter({ (line) -> Bool in
+            
+            if readyToRead {
+                
+                if endOfData {
+                    return false
+                } else {
+                    if line.hasPrefix("*") {
+                        endOfData = true
+                        return false
+                    } else {
+                        return true
+                    }
+                }
+                
+            } else {
+                if line.hasPrefix("-") {
+                    readyToRead = true
+                }
+                return false
+            }
+        })
+        
+        var dataInts = dataLines
+            .map { $0.components(separatedBy: " ") }
+            .map({ (array) -> [Int] in
+                return array.flatMap{ Int( $0.trimmingCharacters(in: .whitespaces)) }
+            })
+        
+    } catch {
+        print(error)
+    }
+
