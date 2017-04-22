a3a8f09043ab9ce2c85088fea8f137ec75bb89f4
Maciej Krolikowski <mkkrolik@gmail.com>
2017-04-22 16:37:29 +0200

    Initial Commit

---
+++ FileParser/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FileParser
+//
+//  Created by Maciej Krolikowski on 22/04/2017.
+//  Copyright © 2017 Maciej Krolikowski. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
